6365fe48087cc5844f16d4bbc5e0be4d95ffcfc4
DESKTOP-4KERMI6\tyron <mail@tyronn.me>
2021-02-09 13:15:40 -0800

    UI update

--- js/scripts.js
+++ js/scripts.js
@@ -4,9 +4,36 @@ class Init extends React.Component {
 
 	constructor(props) {
     super(props);
+		this.state = {
+			page : "landing"
+		}
+  }
+
+	componentDidMount() {
+
+	}
+
+	render() {
+		if ( this.state.page == "landing" ) {
+			return(
+				<div className="container-fluid" id="appRow">
+					<Landing />
+				</div>
+			)
+		}
+	}
+
+}
+
+class Landing extends React.Component {
+
+	constructor(props) {
+    super(props);
     this.state = {
 			boxes : null
 		};
+
+		this.hoverState = this.hoverState.bind(this);
   }
 
 	componentDidMount() {
@@ -26,15 +53,22 @@ class Init extends React.Component {
 						let boxArrCheck = [];
 						let objL = obj.length;
 
+						console.log(obj);
+
 						for(var i = 0; i < objL; i++) {
-							console.log(obj[i].attributes["title"]);
 							if ( boxArrCheck.indexOf(obj[i].attributes["title"]) == -1 ) {
 								boxArrCheck.push(obj[i].attributes["title"]);
-								boxes.push(obj[i].attributes["title"]);
+								boxes.push({
+									number : "0" + ( i + 1 ),
+									title : obj[i].attributes["title"],
+									details : obj[i].attributes["details"]
+								});
 							}
 						}
 
 						$this.setState({boxes : boxes});
+
+						$this.applyBoxClasses();
         }
 
 
@@ -44,9 +78,47 @@ class Init extends React.Component {
 
 	}
 
+	applyBoxClasses() {
+
+		let boxes = document.getElementsByClassName("classCol");
+		let winH = window.innerHeight;
+		for(var i = 0; i < boxes.length; i++) {
+			boxes[i].style.height = winH + "px";
+			if ( i == 0 ) {
+				boxes[i].classList.add("active");
+			} else {
+				boxes[i].classList.add("not_active")
+			}
+		}
+
+	}
+
+	hoverState(event) {
+		console.log("hovered");
+		let boxes = document.getElementsByClassName("classCol");
+		let elemIndex = this.getElementIndex(event.target.parentNode);
+
+		console.log(elemIndex);
+
+		for(var i = 0; i < boxes.length; i++) {
+			if ( i == elemIndex ){
+				boxes[i].classList.remove("not_active");
+				boxes[i].classList.add("active");
+			} else {
+				boxes[i].classList.remove("active");
+				boxes[i].classList.add("not_active");
+			}
+		}
+
+	}
+
+	getElementIndex(element) {
+  	return Array.from(element.parentNode.children).indexOf(element);
+	}
+
 	render() {
 		const divs = (this.state.boxes || []).map((box) => {
-			return <div className="classCol"><h1 className="display-4">{box}</h1></div>
+			return <div className="classCol" onMouseEnter={this.hoverState}><div className="innerClassCol"><h3 className="h1">{box.number}</h3><div className="boxText"><h2 className="display-4">{box.title}</h2><p>{box.details}</p></div></div></div>
 		});
 		console.log(divs);
 		return(
